822ede2d16fe2780b900f9aab4b90800cf3b1917
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2019-09-17 02:52:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -38,12 +38,14 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
 
   # add 4 gun powder to the shipping_manifest hash below
 
 
 
   # return the shipping_manifest hash below
+  return shipping_manifest
 
 end
